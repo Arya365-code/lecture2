bf726bf6ad87e519931fcb30873becae9b41af04
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-04-29 17:55:01 +1000

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p = Point(3, 5)
+print(p.x)
+print(p.y)
+
+#Should print out 3 and 5
+#In this we created a 'class'

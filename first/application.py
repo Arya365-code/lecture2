7109551b37d836a6fad3d5eeeb07fdd3c8dcefa0
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-01 18:03:50 +1000

    Update and rename application.py to first/application.py

--- first/application.py
+++ first/application.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  return "Hello, world!"
 
 @app.route("/<string:name>")
 def hello(name):

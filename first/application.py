43b185d133a4381bdffcc33714ad564f14dd5d48
Arya365-code <64459771+Arya365-code@users.noreply.github.com>
2020-05-01 18:04:06 +1000

    Update application.py

--- first/application.py
+++ first/application.py
@@ -6,11 +6,5 @@ app = Flask(__name__)
 def index():
   return "Hello, world!"
 
-@app.route("/<string:name>")
-def hello(name):
-  name = name.capitalize()
-  return f"<h1>Hello, {name}!<h1>"
-
 #So if you go to the default route which just ends in a /
-#the page will show "Hello, world!". But if you add '/name'
-#to the end of the URL, it will show 'Hello, name!'
+#the page will show "Hello, world!". 
